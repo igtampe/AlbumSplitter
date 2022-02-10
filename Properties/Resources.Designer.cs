ad42e4a211eb166ead598257510bb0942b84a19a
Ignacio Tampe <Igtampe@gmail.com>
2022-02-10 16:22:10 -0400

    Add AS Icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace AlbumSplitter.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon ASIcon {
+            get {
+                object obj = ResourceManager.GetObject("ASIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Checkmark {
